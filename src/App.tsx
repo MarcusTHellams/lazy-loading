30d49b9f8de8c16cac7a8ffb624a232ab90f1087
Marcus Hellams <mhellams@hotmail.com>
2023-06-10 09:56:18 -0400

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
 import { useIntersection } from '@mantine/hooks';
+import * as Portal from '@radix-ui/react-portal';
 import { useInfiniteQuery } from '@tanstack/react-query';
 import axios from 'axios';
 import { useEffect, useMemo, useRef } from 'react';
+import Loader from 'react-loaders';
+import { useLockBodyScroll } from 'react-use';
 
 interface Post {
   userId: number;
@@ -42,6 +45,8 @@ function App() {
     // } as unknown as InfiniteData<Post>,
   });
 
+  useLockBodyScroll(isFetchingNextPage);
+
   const lastPostRef = useRef<HTMLElement>(null);
   const { entry, ref } = useIntersection({
     root: lastPostRef.current,
@@ -62,35 +67,43 @@ function App() {
   }, [data?.pages]);
 
   return (
-    <div className="container mx-auto mt-16">
-      <div className="prose max-w-none">
-        <h1>Posts</h1>
-        {data ? (
-          <>
-            <ul className="not-prose pl-0 divide-y divide-gray-200 rounded-xl border border-gray-200 shadow-sm list-none">
-              {_pages?.map((post, index) => {
-                return (
-                  <li
-                    ref={
-                      index === _pages.length- 1
-                        ? ref
-                        : undefined
-                    }
-                    key={post.id}
-                    className="p-4"
-                  >
-                    <h4 className="text-lg font-medium leading-loose">
-                      {post.id}: {post.title}
-                    </h4>
-                    <p className="text-gray-500">{post.body}</p>
-                  </li>
-                );
-              })}
-            </ul>
-          </>
-        ) : null}
+    <>
+      <div className="container mx-auto mt-16">
+        <div className="prose max-w-none">
+          <h1>Posts</h1>
+          {data ? (
+            <>
+              <ul className="not-prose pl-0 divide-y divide-gray-200 rounded-xl border border-gray-200 shadow-sm list-none">
+                {_pages?.map((post, index) => {
+                  return (
+                    <li
+                      ref={index === _pages.length - 1 ? ref : undefined}
+                      key={post.id}
+                      className="p-4"
+                    >
+                      <h4 className="text-lg font-medium leading-loose">
+                        {post.id}: {post.title}
+                      </h4>
+                      <p className="text-gray-500">{post.body}</p>
+                    </li>
+                  );
+                })}
+              </ul>
+            </>
+          ) : null}
+        </div>
       </div>
-    </div>
+      {isFetchingNextPage ? (
+        <Portal.Root>
+          <div className="fixed inset-0 z-10 bg-black bg-opacity-50"></div>
+          <div className="fixed inset-0 z-50 flex items-center justify-center p-4 sm:p-0">
+            <div className="mx-auto overflow-hidden rounded-lg bg-transparent ">
+              <Loader type="line-scale" active />
+            </div>
+          </div>
+        </Portal.Root>
+      ) : null}
+    </>
   );
 }
 
